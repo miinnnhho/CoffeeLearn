022cb1e9163ffcedb3fdf48f29c1c95104f49fbf
kwonsukhee <tnzl1020@gmail.com>
2023-07-11 02:01:27 +0900

    test.html의 css넣기

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ import ejs from 'ejs';
 import { fileURLToPath } from 'url';
 
 import indexRouter from './routes/index.js';
+import itemsRouter from './routes/items.js';
+import itemsCategoryRouter from './routes/items_category.js';
+import itemsInfoRouter from './routes/items_info.js';
+import testRouter from './routes/test.js';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
@@ -16,6 +20,7 @@ app.set('view engine', 'html');
 app.engine('.html', ejs.__express);
 
 app.use('/', indexRouter);
+app.use('/test', testRouter);
 
 app.listen(PORT, () => {
   console.log(`✅ Server listening on port ${PORT}`);
