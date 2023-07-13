212f8cf455d0564bab4ef1f068a6ff8e78a7fe31
kwonsukhee <tnzl1020@gmail.com>
2023-07-13 14:01:05 +0900

    feat. 페이지 이동 기능 추가 등

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ import { fileURLToPath } from 'url';
 import indexRouter from './routes/index.js';
 import itemsRouter from './routes/items.js';
 import itemsInfoRouter from './routes/items_info.js';
-import itemsListCoffeeRouter from './routes/items_list_coffee.js';
-import itemsListGiftRouter from './routes/items_list_gift.js';
+import itemsCoffeeRouter from './routes/items_coffee.js';
+import itemsGiftRouter from './routes/items_gift.js';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
@@ -21,8 +21,8 @@ app.engine('.html', ejs.__express);
 
 app.use('/', indexRouter);
 app.use('/items', itemsRouter);
-app.use('/items_list_coffee', itemsListCoffeeRouter);
-app.use('/items_list_gift', itemsListGiftRouter);
+app.use('/items_coffee', itemsCoffeeRouter);
+app.use('/items_gift', itemsGiftRouter);
 app.use('/items_info', itemsInfoRouter);
 
 app.listen(PORT, () => {
--- routes/items_gift.js
+++ routes/items_gift.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  res.render('items_list_coffee');
+  res.render('items_gift');
 });
 
 export default router;
