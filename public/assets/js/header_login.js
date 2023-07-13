86bbc75e399a0c5c75246f3342beaadf0d9dc4a1
[SW5] 조민호 <miinnnhho>
2023-07-13 19:14:31 +0900

    docs. header.html, common.css / feat. 회원정보수정파일.js

--- public/assets/js/header_login.js
+++ public/assets/js/header_login.js
@@ -1,18 +1,22 @@
 function checkLoginStatus() {
-    const auth_btn = document.getElementById('authBtn'); //버튼
-    const isLoggedIn = localStorage.getItem('authToken') !== null; //토큰이 있으면
+    const auth_btn = document.querySelector('.authBtn'); //a tag
+    const btn_span = document.querySelector('.btn-Span'); //버튼
+    const isLoggedIn = localStorage.getItem('userId') !== null; //토큰이 있으면
 
     if (isLoggedIn) {
-        auth_btn.textContent = '로그아웃'; //로그아웃로 글 변경
-        auth_btn.onclick = () => { //클릭하면
-            localStorage.removeItem('authToken'); //토큰 제거
+        btn_span.textContent = '로그아웃'; //로그아웃로 글 변경
+        auth_btn.onclick = () => {
+            //a태그전체 클릭하면
+            localStorage.removeItem(''); //토큰 제거 -> 로그아웃
             alert('로그아웃이 되었습니다.'); //알림
+            location.href = '/'; //홈으로 이동함
             checkLoginStatus();
         };
     } else {
-        auth_btn.textContent = '로그인'; 
+        btn_span.textContent = '로그인';
         auth_btn.onclick = () => {
-            location.href = '/login';
+            //a태그전체 클릭하면
+            location.href = '/login'; //login.html로 넘어감
         };
     }
 }
--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -5,11 +5,50 @@ const userName = document.getElementById('name');
 const userPhone_number = document.getElementById('phoneNumber');
 const userAddress = document.getElementById('address');
 const userdetailedAddress = document.getElementById('detailedAddress');
+
 const submitBtn = document.querySelector('.btn1');
 
-submitBtn.addEventListener('click', register);
+// submitBtn.addEventListener('click', register);
 
-function register() {
+// async function register() {
+// const req = {
+//     email: userId.value,
+//     password: userPw.value,
+//     // passwordCheck: userPwCheck.value, 필요없으면 빼기
+//     name: userName.value,
+//     phone: userPhone_number.value,
+//     addr: userAddress.value + ' ' + userdetailedAddress.value,
+// };
+// console.log(req);
+//     try {
+//         const responce = await fetch('/user/signup', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify(req), //{ email, password, name, phone, addr }
+//         });
+//         // .then((res) => res.json())
+//         // .then((res) => {
+//         const data = await responce.json();
+
+//         if (data.success) {
+//             localStorage.setItem('userId', JSON.stringify(req));
+//             location.href = 'finish_signup'; //성공하면 finish_signup으로 이동
+//         } else {
+//             alert(data.msg);
+//         }
+//     } catch (err) {
+//         console.error('회원가입 중 에러 발생');
+//         alert('회원가입 중 에러 발생');
+//     }
+// }
+
+
+
+
+// 이거로 로컬에 회원가입정보 들어가는건 확인함
+submitBtn.addEventListener('click', function () {
     const req = {
         email: userId.value,
         password: userPw.value,
@@ -19,23 +58,6 @@ function register() {
         addr: userAddress.value + ' ' + userdetailedAddress.value,
     };
     console.log(req);
-    fetch('/user/signup', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(req),
-    })
-        .then((res) => res.json())
-        .then((res) => {
-            if (res.success) {
-                location.href = 'finish_signup'; //성공하면 finish_signup으로 이동
-            } else {
-                alert(res.msg);
-            }
-        })
-        .catch((err) => {
-            console.error('회원가입 중 에러 발생');
-            alert('회원가입 중 에러 발생');
-        });
-}
+    localStorage.setItem('userId', JSON.stringify(req));
+    location.href = 'finish_signup';
+});
