256b7630a95fb17f8ffec636348f6146cf13e1fe
[SW5] 조민호 <miinnnhho>
2023-07-15 05:04:59 +0900

    docs. 로그인 이후 표시 에러  수정완료

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -11,13 +11,13 @@ function alertMessage() {
 function checkLoginStatus() {
     const auth_btn = document.querySelector('.auth-btn'); //a tag
     const btn_span = document.querySelector('.btn-Span'); //버튼
-    const isLoggedIn = localStorage.getItem('userId') !== null; //토큰이 있으면
+    const isLoggedIn = localStorage.getItem('token') !== null; //토큰이 있으면
 
     if (isLoggedIn) {
         btn_span.textContent = '로그아웃'; //로그아웃로 글 변경
         auth_btn.onclick = () => {
             //a태그전체 클릭하면
-            localStorage.removeItem(''); //토큰 제거 -> 로그아웃
+            localStorage.removeItem('token'); //토큰 제거 -> 로그아웃
             alert('로그아웃이 되었습니다.'); //알림
             location.href = '/'; //홈으로 이동함
             checkLoginStatus();
--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -2,6 +2,13 @@ const useremail = document.getElementById('id');
 const userpassword = document.getElementById('pw');
 const loginBtn = document.querySelector('.btn1');
 
+
+
+window.addEventListener('load', () => {
+    checkLoginStatus();
+});
+
+//로그인
 window.handleLoginSubmit = function (event) {
     event.preventDefault();
 
@@ -16,35 +23,60 @@ window.handleLoginSubmit = function (event) {
         method: 'POST',
         body: JSON.stringify(loginData),
     })
-        .then((response) => response.json())
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                throw new Error('Login failed');
+            }
+        })
         .then((data) => {
             if (data.token) {
                 // 로그인 성공 후 처리
                 localStorage.setItem('token', data.token);
                 window.location.href = '/'; //홈으로 이동
-            } else {
-                // 로그인 실패 처리
-                alert('회원정보를 찾을 수 없습니다.');
+                checkLoginStatus();
             }
+        })
+        .catch((error) => {
+            // 로그인 실패 처리
+            console.error(error);
+            alert('e-mail 혹은 비밀번호를 재확인해 주세요.');
         });
 
-    //로그인시 로그인 버튼 -> 로그아웃 버튼으로 변함(반대도)
-    document.addEventListener('DOMContentLoaded', function () {
-        const loginBtn = document.getElementById('loginBtn');
-        const logoutBtn = document.getElementById('logoutBtn');
+    // fetchApi('/users/login', {
+    //     method: 'POST',
+    //     body: JSON.stringify(loginData),
+    // })
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //         if (data.token) {
+    //             // 로그인 성공 후 처리
+    //             localStorage.setItem('token', data.token);
+    //             window.location.href = '/'; //홈으로 이동
+    //         } else {
+    //             // 로그인 실패 처리
+    //             alert('회원정보를 찾을 수 없습니다.');
+    //         }
+    //     });
 
-        const token = localStorage.getItem('token');
+    // //로그인시 로그인 버튼 -> 로그아웃 버튼으로 변함(반대도)
+    // document.addEventListener('DOMContentLoaded', function () {
+    //     const loginBtn = document.getElementById('loginBtn');
+    //     const logoutBtn = document.getElementById('logoutBtn');
 
-        if (token) {
-            // 로그인 상태일 때
-            loginBtn.style.display = 'none';
-            logoutBtn.style.display = 'block';
-        } else {
-            // 로그아웃 상태일 때
-            loginBtn.style.display = 'block';
-            logoutBtn.style.display = 'none';
-        }
-    });
+    //     const token = localStorage.getItem('token');
+
+    //     if (token) {
+    //         // 로그인 상태일 때
+    //         loginBtn.style.display = 'none';
+    //         logoutBtn.style.display = 'block';
+    //     } else {
+    //         // 로그아웃 상태일 때
+    //         loginBtn.style.display = 'block';
+    //         logoutBtn.style.display = 'none';
+    //     }
+    // });
     //로그아웃 처리
     function logout() {
         localStorage.removeItem('token');
