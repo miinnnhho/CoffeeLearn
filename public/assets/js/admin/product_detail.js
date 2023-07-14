b6cc1446a9f4696fb5f61910a5e5917c7c3c92ac
[SW5] 이혜진 <meggylee792@gmail.com>
2023-07-14 18:02:37 +0900

    Merge branch 'new-branch' of https://kdt-gitlab.elice.io/sw_track/class_05/web_project/team07/front-end into feature/admin

--- public/assets/js/admin/product_detail.js
+++ public/assets/js/admin/product_detail.js
@@ -1,68 +1,40 @@
-// 서버에서 아이디에 해당하는 상품 정보 가져오기
-async function fetchProductDetails(productId) {
-  try {
-      const res = await fetch('/assets/data/products.json', {
-          method: 'GET',
-      });
-      if (!res.ok) {
-          throw new Error('에러가 발생했습니다.');
-      }
-      const productList = await res.json();
-      const productDetails = productList.find((product) => product.id === productId);
-      console.log(productDetails)
-      return productDetails;
-  } catch (error) {
-      console.error(error);
-      // 오류 처리
-      return null; // 오류 발생 시 null 반환
-  }
+const productId = window.location.pathname.split('/').at(-1);
+
+fetch('/assets/data/products.json')
+    .then((response) => response.json())
+    .then((data) => {
+        // 상품 목록 중 찾고자 하는 상품을 id로 필터링하여 선택합니다.
+        const product = data.filter((item) => {
+            return item.id === Number(productId);
+        })[0];
+
+        // HTML 폼 요소를 선택하거나 생성하여, 상품 정보를 채웁니다.
+        document.querySelector('.select-category').value = product.category;
+        document.querySelector('.select-taste').value = product.taste;
+        document.querySelector('.select-origin').value = product.origin;
+        document.querySelector('.input-name').value = product.name;
+        document.querySelector('.input-price').value = product.price;
+        //document.querySelector('.input-amount').value = product.amount;
+        //document.querySelector('.input-main-img').value = product.mainImg;
+       // document.querySelector('.input-sub-img').value = product.subImg;
+        document.querySelector('.input-description').value = product.description;
+        //document.querySelector('.select-show').value = product.show;
+        checkCategory(product.category);
+    })
+    .catch((error) => console.log(error));
+
+function handleChangeCategory() {
+    var select = document.querySelector('.select-category');
+
+    checkCategory(select.value);
 }
 
-// 페이지 로드 시 폼 채우기 함수 실행
-window.addEventListener('DOMContentLoaded', async () => {
-  const productId = window.location.pathname.split('/').at(-1);
-  console.log(productId);
-  const productDetails = await fetchProductDetails(productId);
-
-  if (productDetails) {
-      const categorySelect = document.querySelector('.select-category');
-      const tasteSelect = document.querySelector('.select-taste');
-      const originSelect = document.querySelector('.select-origin');
-      const nameInput = document.querySelector('.input-name');
-      const priceInput = document.querySelector('.input-price');
-      const amountInput = document.querySelector('.input-amount');
-      const mainImgInput = document.querySelector('.input-main-img');
-      const subImgInput = document.querySelector('.input-sub-img');
-      const descriptionInput = document.querySelector('.input-description');
-      const showSelect = document.querySelector('.select-show');
-
-      // 카테고리 옵션 설정
-      const categoryOptions = productDetails.categories.map((category) => {
-          return `<option value="${category}" ${category === productDetails.category ? 'selected' : ''}>${category}</option>`;
-      });
-      categorySelect.innerHTML = categoryOptions.join('');
-
-      // 맛 옵션 설정
-      const tasteOptions = productDetails.tastes.map((taste) => {
-          return `<option value="${taste}" ${taste === productDetails.taste ? 'selected' : ''}>${taste}</option>`;
-      });
-      tasteSelect.innerHTML = tasteOptions.join('');
-
-      // 원산지 옵션 설정
-      const originOptions = productDetails.origins.map((origin) => {
-          return `<option value="${origin}" ${origin === productDetails.origin ? 'selected' : ''}>${origin}</option>`;
-      });
-      originSelect.innerHTML = originOptions.join('');
-
-      // 나머지 입력 필드 값 설정
-      nameInput.value = productDetails.name;
-      priceInput.value = productDetails.price;
-      amountInput.value = productDetails.amount;
-      mainImgInput.value = productDetails.mainImg;
-      subImgInput.value = productDetails.subImg;
-      descriptionInput.value = productDetails.description;
-
-      // 상품 표시 옵션 설정
-      showSelect.value = productDetails.show;
-  }
-});
+function checkCategory(target) {
+    if (target === '커피') {
+        document.querySelector('.select-taste').disabled = false;
+        document.querySelector('.select-origin').disabled = false;
+    } else {
+        document.querySelector('.select-taste').disabled = true;
+        document.querySelector('.select-origin').disabled = true;
+    }
+}
