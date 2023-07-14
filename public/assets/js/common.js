2e541d8f823993598e5b3710f5e6a1c6626fc565
[SW5] 조민호 <miinnnhho>
2023-07-15 03:57:21 +0900

    docs. 회원가입

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -9,9 +9,9 @@ function alertMessage() {
 
 // 로그인, 로그아웃 텍스트 및 링크 전환
 function checkLoginStatus() {
-  const auth_btn = document.querySelector('.auth-btn'); //a tag
-  const btn_span = document.querySelector('.btn-Span'); //버튼
-  const isLoggedIn = localStorage.getItem('userId') !== null; //토큰이 있으면
+    const auth_btn = document.querySelector('.auth-btn'); //a tag
+    const btn_span = document.querySelector('.btn-Span'); //버튼
+    const isLoggedIn = localStorage.getItem('userId') !== null; //토큰이 있으면
 
     if (isLoggedIn) {
         btn_span.textContent = '로그아웃'; //로그아웃로 글 변경
@@ -26,10 +26,10 @@ function checkLoginStatus() {
         btn_span.textContent = '로그인';
         auth_btn.onclick = () => {
             //a태그전체 클릭하면
-            location.href = 'login'; //login.html로 넘어감
+            location.href = '/login'; //login.html로 넘어감
         };
     }
-  }
-  window.addEventListener('load', () => {
-      checkLoginStatus();
-  });
+}
+window.addEventListener('load', () => {
+    checkLoginStatus();
+});
